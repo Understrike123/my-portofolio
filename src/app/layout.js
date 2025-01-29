90d7a2b3718c69e37e237584df0a0ddaa27f85ef
muhilhamfebr26 <muhilhamfebr2@gmail.com>
2025-01-30 01:44:52 +0700

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
+import { ThemeProvider } from "@material-tailwind/react";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
