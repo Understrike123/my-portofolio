c7d26c8b389bcab7e75fb98cc2311efb8e5b7f82
muhilhamfebr26 <muhilhamfebr2@gmail.com>
2025-01-30 02:10:26 +0700

    benerin config error

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
-const withMT = require("@material-tailwind/react/utils/withMT");
-module.exports = withMT({
+export default {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "path-to-your-node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
-    "path-to-your-node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -17,4 +14,4 @@ module.exports = withMT({
     },
   },
   plugins: [],
-});
+};
